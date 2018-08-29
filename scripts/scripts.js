dafc26ec4803d9741ca03f6650bd7ca78ff31a5e
ma mica trixia busog encela <932400139@cit3-501m-14.acg.ac.nz>
2018-08-29 15:31:03 +1200

    Halfway through gallery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,47 @@
-$(function(){
+$(function() {
 
-	// Open and close nav on mobile
-	$('.bars').on('click',function(e){
+    // Open and close nav on mobile
+    $('.bars').on('click', function(e) {
 
-		var navData = $('.navigation').data('nav');
+        var navData = $('.navigation').data('nav');
 
-		e.stopPropagation();
-		
-		if(navData == 'close'){
-			$('.navigation').addClass('nav-open')
-							.data('nav', 'open')
+        e.stopPropagation();
 
-			$('.bars>i').first().removeClass('fas fa-bars')
-							.addClass('fas fa-times');
+        if (navData == 'close') {
+            $('.navigation').addClass('nav-open')
+                .data('nav', 'open')
 
-      $('.heading,.sub-heading').addClass('text-hide');
-		}
+            $('.bars>i').first().removeClass('fas fa-bars')
+                .addClass('fas fa-times');
 
-		else {
-			$('.navigation').removeClass('nav-open')
-							.data('nav', 'close');
-			$('.bars>i').removeClass('fas fa-times')
-						.addClass('fas fa-bars');
+            $('.heading,.sub-heading').addClass('text-hide');
+        } else {
+            $('.navigation').removeClass('nav-open')
+                .data('nav', 'close');
+            $('.bars>i').removeClass('fas fa-times')
+                .addClass('fas fa-bars');
 
-      $('.heading,.sub-heading').removeClass('text-hide');
-		}
-	});
+            $('.heading,.sub-heading').removeClass('text-hide');
+        }
+    });
+
+    //Staff Gallery
+    let key = 'LTLUQQXSYWEe5kN6NqJ1DgcVydeNXBFq';
+
+
+    let urlProjects = 'https://api.behance.net/v2/users/heycheese/projects?client_id=' + key;
+
+    console.log(urlProjects);
+
+    if ($('#staff').length > 0) {
+        $.ajax({
+            url: urlProjects,
+            dataType: 'jsonp',
+            success: function(res) {
+                _(res.projects).each(function(project) {
+                    $('<div class="staff-work-tile" style="background-image: url('+ project.covers.original +');">' + '<div class="tile-title"> <span>' + project.name + '</span></div></div>').appendTo('.staff-work');
+                });
+            }
+        });
+    }
 });
