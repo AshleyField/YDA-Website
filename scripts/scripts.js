9a3b9633d82996afdc158affc22f3585e2db7040
Ashley Field <ashleyfield@Ashleys-MacBook-Pro.local>
2018-08-31 12:38:34 +1200

    completed staff page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,25 +25,8 @@ $(function() {
         }
     });
 
-    //animation of hero home
-
-    var typed = new Typed('#finished', {
-        stringsElement: '#typing',
-        showCursor: false,
-        startDelay: 1000,
-        backDelay: 2000,
-        backSpeed:60,
-        typeSpeed:60,
-        loop: true,
-        loopCount: Infinity
-    });
-
     //Staff Gallery
     let key = 'IuFvFQ4A5CUDOm6XyKKvzFOccCApTNNZ';
-
-    let urlProjects = 'https://api.behance.net/v2/users/heycheese/projects?client_id=' + key;
-
-    console.log(urlProjects);
 
     var users = ['andrewcouldwell', 'AlexSeagull', 'juliettewang']
 
@@ -51,6 +34,21 @@ $(function() {
     var projectTemplate = Template7(projectHTML).compile();
 
     if ($('#index').length > 0) {
+
+
+        //animation of hero home
+
+        var typed = new Typed('#finished', {
+            strings: ['A Digital Design Agency','We build winning brands'],
+            showCursor: false,
+            startDelay: 1000,
+            backDelay: 2000,
+            backSpeed:60,
+            typeSpeed:60,
+            loop: true,
+            loopCount: Infinity
+        });
+
 
         _(users).each(function(username){
             var profileUrl = 'https://api.behance.net/v2/users/'+ username +'/?client_id=' + key;
@@ -62,7 +60,7 @@ $(function() {
 
                     console.log(res);
 
-                    var output = projectTemplate(res);
+                    let output = projectTemplate(res);
                     $('.l_column_container').append(output);
                 }
             })
@@ -70,13 +68,64 @@ $(function() {
     }
 
     if ($('#staff').length > 0) {
+
+        var staffHeroHTML = $('#staffHeroTemplate').text();
+        var staffHeroTemplate = Template7(staffHeroHTML).compile();
+
+        var urlParams = new URLSearchParams(window.location.search);
+
+        var staffID = urlParams.get('id')
+
+        console.log(staffID);
+
+        var staffUrlProjects = 'https://api.behance.net/v2/users/'+ staffID +'/projects?client_id=' + key;
+        var staffUrl = 'https://api.behance.net/v2/users/'+ staffID +'?client_id=' + key;
+
         $.ajax({
-            url: urlProjects,
+            url: staffUrl,
             dataType: 'jsonp',
             success: function(res) {
+
+                var fields = res.user.fields;
+
+                var fieldsOutput = _(fields).reduce(function(output,item,i,l){ 
+
+                    return i<(l.length-1)? output+ item + ', ' : output+ item;
+
+                },'')
+
+                $('.fields-work').html(fieldsOutput);
+                var section = res.user.sections;
+
+                var text = section[Object.keys(section)[0]];
+
+                $('.staff-bio-inner').html(text);
+
+                console.log(res.user.fields);
+                console.log(res);
+
+                let output = staffHeroTemplate(res);
+                $('.hero').append(output);
+
+
+                $('.staff-hero-fields').html(fieldsOutput);
+            }
+        });
+
+        $.ajax({
+            url: staffUrlProjects,
+            dataType: 'jsonp',
+            success: function(res) {
+
+                var userProjects = res;
                 _(res.projects).each(function(project) {
+
                     $('<div class="staff-work-tile"><div class="staff-work-image" style="background-image: url('+ project.covers.original +');"> </div>' + '<div class="tile-title"> <span>' + project.name + '</span></div></div>').appendTo('.staff-work');
                 });
+
+                console.log(res);
+
+                $('.hero').css('background-image', 'url('+ res.projects["0"].covers.original+ ')')
             }
         });
     }
